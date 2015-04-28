637aae0a03462439b0194f21f294f7584db94795
Brandon Sprague <brandon00sprague@gmail.com>
2015-04-28 16:28:38 -0400

    PCA representation of raw data

--- pca.py
+++ pca.py
@@ -1,13 +1,17 @@
 import numpy as np
-from skimage.io import imsave
-from skimage.transform import rescale
 from sklearn.decomposition import PCA
 
-data = np.loadtxt("rounds.csv", dtype=int)
+filename = "rounds_sample.csv"
+data = np.loadtxt(filename, dtype=int)
+
 # Our new representation is the number of data cases by our new basis of
 # 10-elements per round, plus the label
-ndata = np.zeros([len(data), 10 * 100 + 1)])
+ndata = data[:,0]
+ndata = ndata.reshape((len(ndata), 1))
 for i in range(100):
+    print("PCA on round", i)
     dr = PCA(n_components=10)
     # Fit to one round of positions
-    dr.fit_transform(data[:,(1 + 289 * i):(1 + 289 * (i + 1))])
+    ndata = np.hstack((ndata, dr.fit_transform(data[:,(1 + 289 * i):(1 + 289 * (i + 1))])))
+
+np.save(filename.split(".")[0] + ".npy", ndata)
